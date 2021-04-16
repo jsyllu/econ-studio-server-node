e158fbff53bb5a2833e30cd13810f1179456cbe1
jsyllu <32682105+jsyllu@users.noreply.github.com>
2021-04-15 23:20:50 -0400

    update question service

--- services/questions/question-service.js
+++ services/questions/question-service.js
@@ -1,24 +1,20 @@
-const questions = require('./questions.json')
+const questionsDao = require('../../daos/questions-dao')
 
 const createQuestion = () => {
 
 }
 const createQuestionForQuiz = (qzid, question) => {
-    question.quizId = qzid
-    questions.add(question)
+    // question.quizId = qzid
+    // questions.add(question)
 }
 const findAllQuestions = () => {
-    return questions
+    return questionsDao.findAllQuestions()
 }
 const findQuestionsForQuiz = (qzid) => {
-    return questions.filter((question) => {
-        return (question.quizId === qzid)
-    })
+    return questionsDao.findQuestionsForQuiz(qzid)
 }
 const findQuestionById = (qsid) => {
-    return questions.find((q) => {
-        return (q._id === qsid)
-    })
+    return questionsDao.findQuestionById(qsid)
 }
 const updateQuestion = (qid) => {
 
