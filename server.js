33feeb311ec1eb1beb4c01b1edfb1f54854e78e5
jsyllu <32682105+jsyllu@users.noreply.github.com>
2021-04-15 23:17:11 -0400

    update server

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3001
+require('dotenv').config()
+const MONGODB_URI = process.env.ATLAS_URI
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+const mongoose = require('mongoose')
+mongoose.connect(MONGODB_URI,
+    // 'mongodb://localhost:27017/econ-studio',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Connected to mongodb atlas successfully')
+    })
+    .catch(err => console.log(err))
+
+const session = require('express-session')
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    // cookie: {secure: true}
+}))
 
 // configure CORS
 app.use(function (req, res, next) {
@@ -12,11 +38,9 @@ app.use(function (req, res, next) {
     next()
 })
 
-// const demos = require('./controllers/demo-controller')
-// demos(app)
-
 require('./controllers/quiz-controller')(app)
 require('./controllers/question-controller')(app)
+require('./controllers/quiz-attempt-controller')(app)
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
